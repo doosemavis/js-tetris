27bc2d41022d5b68d85ba5c08d07aa3430552e53
Moose <Davis>
2021-04-24 17:56:11 -0500

    Added .mini-grid + .container

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.keyCode === 37) {
             moveLeft();
         } else if (e.keyCode === 38) {
-            // rotate()
+            rotate();
         } else if (e.keyCode === 39) {
             moveRight()
         } else if (e.keyCode === 40) {
@@ -137,5 +137,17 @@ document.addEventListener('DOMContentLoaded', () => {
         draw();
     }
 
+    // rotate the tetromino
+    function rotate() {
+        undraw();
+        currentRotation ++
+        if (currentRotation === current.length) { // if the current oration gets to 4, make it go back to 0
+            currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
+        draw();
+    }
+
+    
 
 })
